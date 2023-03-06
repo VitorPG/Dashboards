b0f6d7f1a744ffc1611256948aa0ff93f3dff8c7
Vitor Guerra <vitorpbpguerra@hotmail.com>
2023-03-06 12:32:12 -0300

    set comparison based on choice

--- capstone/app.py
+++ capstone/app.py
@@ -7,21 +7,23 @@ df=pd.read_csv('data\dataset.csv',sep=',')
 df = df.drop(['Unnamed: 0','key','mode','tempo','time_signature','loudness','duration_ms','explicit'],axis=1)
 
 grades=df.copy()
-#Transformando os valores das colunas em int
+#Changing columns values to int
 grades[['danceability','energy','speechiness','acousticness','instrumentalness','liveness','valence']]=round(grades[['danceability','energy','speechiness','acousticness','instrumentalness','liveness','valence']]*100,ndigits=None).astype(int)
 
 grade_dic={range(0,20):'E',range(20,40):'D',range(40,60):'C',range(60,80):'B',range(80,101):'A'}
-#Substituindo os valores por letras, conforme graduação acima
+#Replacing numbers for letter grades 
 grades[['popularity','danceability','energy','speechiness','acousticness','instrumentalness','liveness','valence']]=grades[['popularity','danceability','energy','speechiness','acousticness','instrumentalness','liveness','valence']].replace(grade_dic)
 
-#gerando coluna com musica e artistas para eliminar repetições
+#creating info column to avoid duplicates in same song/artist but keep same song name for diferent artists
 grades['infor']=grades.track_name+", " + grades.artists
+
+#drop duplicates from same song in the same genre
 grades.drop_duplicates(subset=['infor','track_genre'],inplace=True)
 
 st.set_page_config(page_title='What to play')
 header=st.container()
 inputs=st.container ()
-sugestions=st.container()
+recomend=st.container()
 caracteristics=st.container()
 
 with header:
@@ -42,13 +44,39 @@ with inputs:
 
 choice=grades[grades['infor']==music].reset_index(drop=True)
 
-print(choice)
-pop_sug=grades[(grades['popularity']=='A') & (grades['danceability']==choice['danceability'][0])]
-unpop_sug= grades[(grades['popularity']=='E') & (grades['danceability']==choice['danceability'][0])]
+#choose first row from choice options (avoid same song in diferent genders)
+match=pd.DataFrame(choice.iloc[:1])
 
-with sugestions:
+#print(match)
+
+#filter possible sugestions only to same genre
+genre_df=grades[grades['track_genre']==genre]
+
+sugestions=[]
+
+for row in range(genre_df.shape[0]):
+    
+    linha=genre_df.iloc[[row]].reset_index(drop=True)
+    
+    #compare similarity between to chosen music and all the rows inside genre sample
+    diference=match.compare(linha,align_axis=0)
+
+    # since all duplicates from same genre have been dropped already, 
+    # this diference of less than 10 will result in matches of at least 4 of the following parameters:
+    # danceability energy speechiness acousticness instrumentalness liveness valence
+    if diference.shape[1]<=10:
+        sugestions.append(linha)
+
+sugestions=pd.concat(sugestions,axis=0)
+sugestions.reset_index(drop=True,inplace=True)
+
+pop_sug=sugestions[sugestions['popularity']=='A']
+unpop_sug= sugestions[sugestions['popularity']!='A']
+
+with recomend:
+    
     st.write('Top Picks 4U')
-    top_picks=st.table(pop_sug['infor'].head())
+    top_picks=st.table(pop_sug['infor'].head(n_samples))
 
     st.write('U may also like')
-    low_picks= st.table(unpop_sug['infor'].head())
+    low_picks= st.table(unpop_sug['infor'].head(n_samples))
